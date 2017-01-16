7d0222113e53de5d92935851d8e70d815401f422
sameh <sameh.magdy.tawfik@gmail.com>
2017-01-16 14:38:02 -0800

    ganttchart update

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import { GanttChartComponent } from './gantt-chart/gantt-chart.component';
+import { BarchartComponent } from './gantt-chart/barchart/barchart.component';
+import { Gantt } from "./gantt-chart/ganttModel"
+
+export declare class ChartModule {
+}
+export {BarchartComponent,GanttChartComponent,Gantt  };
